162bbf650f9b72abe04ab9482d06dce030ab79c9
KarolSerkowski <33605416+KarolSerkowski@users.noreply.github.com>
2019-03-05 12:16:51 +0100

    first

---
+++ Manage Employees Project_KS/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manage_Employees_Project_KS 
+{
+    
+    class Person
+    {
+        private string name;
+        private string surname { get; set; }
+        
+    }
+
+    class Employee : Person
+    {
+        public string Position { get; set; }
+        public static decimal HolidayBonus { get; set; } = 1000;
+
+        enum Wage
+        {
+            basic,
+            bonus,
+            other
+        }
+
+    }
+
+    class Operation
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
